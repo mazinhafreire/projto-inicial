1439f758ba6d1b544f38fb488018270aef3c67ff
mazinhafreire <mariafreire.744156@gmail.com>
2024-03-18 08:26:33 -0300

    formulario

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 var botao = document.getElementById("botao")
 botao.addEventListener("click", alertar);
 
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email=document.getElementById("email")
+var email=document.getElementById("Telefone")
+var email=document.getElementById("CEP")
+var email=document.getElementById("Logradou")
+var email=document.getElementById("Numero")
+var email=document.getElementById("Complemeto")
+var email=document.getElementById("Bairro")
+var email=document.getElementById("Cidade")
+var email=document.getElementById("Estado")
+
 function alertar(){
-    alert("o botao foi clicado!!!")
-}
+    //alert("o nome inserido foi" + nome);
+
+   saida.innerText = "nome:" + nome.value +
+   "\n E-mail:" +email.value
+   "\n E-mail:" + Telefone.value
+   "\n E-mail:" + cep.value
+   "\n E-mail:" + logradou.value
+   "\n E-mail:" + numero.value
+   "\n E-mail:" + complemento.value
+   "\n E-mail:" + Bairro.value
+   "\n E-mail:" + Cidade.value
+   "\n E-mail:" + Estado.value
+
+   ;
+
+}  
 
